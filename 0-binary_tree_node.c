d694710962e4e6778b947452513da3a085e116d5
Naouali <elnaoualinebil@gmail.com>
2020-05-25 09:56:50 +0100

    parent not ull

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,8 +14,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	if (new == NULL)
 	return (NULL);
-	if (parent == NULL)
-	parent = new;
 	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
