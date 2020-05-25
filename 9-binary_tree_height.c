28651b1acf491c36fb36ea1106363808298f9f50
Naouali <elnaoualinebil@gmail.com>
2020-05-25 15:24:52 +0100

    two functions

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,6 +2,33 @@
 #include "binary_trees.h"
 #include <stdio.h>
 /**
+ * tree_height - function tomesure BT height
+ * @node: binary tree to be mesured
+ * Return: max_height
+ */
+
+size_t tree_height(const binary_tree_t *node)
+{
+	size_t i = 0;
+	size_t j = 0;
+	
+	if (node->left)
+	{
+		i = binary_tree_height(node->left);
+	}
+	if (node->right)
+	{
+		j = binary_tree_height(node->right);
+	}
+	if (i > j)
+	{
+		return (i + 1);
+	}
+	return (j + 1);
+
+}
+
+/**
  * binary_tree_height - function tomesure BT height
  * @tree: binary tree to be mesured
  * Return: height
@@ -9,23 +36,11 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t i = 0;
-	size_t j = 0;
+	size_t k = 0;
 
 	if (tree == NULL)
-	{
 		return (0);
-	}
-	else
-	{
-		i = binary_tree_height(tree->left);
-		j = binary_tree_height(tree->right);
-		if (j > i)
-		{
-			return (j + 1);
-		}
-		else
-			return (i + 1);
-	}
+	k = tree_height(tree);
+	k--;
+	return (k);
 }
-
