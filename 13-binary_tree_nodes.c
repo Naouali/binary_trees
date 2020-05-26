dce9f8a69816800f6c0fda79e9df286ddae14f04
Naouali <elnaoualinebil@gmail.com>
2020-05-26 13:48:34 +0100

    number of atleast one subtree

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,12 +1,37 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - function to calculta nodes with at least 1 subtree
- * @tree: pointer to root of the tree
- * Return: number of nodes with at least one subtree
+ * nodes - count nodes of a binary tree node.
+ * @node: pointer to the node to count.
+ * @number: number of nodes
+ * Return: void
  */
+void nodes(const binary_tree_t *node, size_t *number)
+{
+	if (node != NULL && (node->right != NULL || node->left != NULL))
+	{
+		*number += 1;
+		nodes(node->right, number);
+		nodes(node->left, number);
+	}
+}
 
+/**
+ * binary_tree_nodes - function to calculate nodes with at least 1 subtree
+ * @tree: pointer to the root of tree.
+ * Return: number of nodes
+ */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	return (binary_tree_size(tree) - binary_tree_leaves(tree));
+	size_t i = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->right != NULL || tree->left != NULL)
+	{
+		i += 1;
+		nodes(tree->left, &i);
+		nodes(tree->right, &i);
+	}
+	return (i);
 }
